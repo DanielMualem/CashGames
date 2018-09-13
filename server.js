8a794d513b8c904b04e08d13197ea6da644efa0f
DanielMualem <dani.mualem@gmail.com>
2018-09-13 16:52:02 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 
-var html = fs.readFileSync('index.html');
+var html = fs.readFileSync('hextris.html');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
